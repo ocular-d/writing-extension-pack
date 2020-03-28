c8c27ef4fe72c756b86a5d63222ebc71614464ea
ocular-d <sven@ocular-d.tech>
2020-03-28 15:51:52 +0100

    chore(docs): start wih vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,9 @@
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/ocular-d/writing-extension-pack' }
+    ]
+  }
 }
